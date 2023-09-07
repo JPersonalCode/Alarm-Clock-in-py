0f3754a642b6f8b119f2a582e9c31977d3075998
JPersonalCode <143402072+JPersonalCode@users.noreply.github.com>
2023-09-07 12:38:29 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import time
+import os
 
 class Clock():
     def __init__(self):
@@ -32,23 +33,41 @@ class Clock():
                 print(self.Alarm_List)
                 print("Current Time is: " +self.Current_Time_Update())
                 self.Set_Alarm()
+    def Remove_Alarm(self):
+        Choice = "'"+input("Please Input Name of Alarm to remove: ")+"'"
+        for x in self.Alarm_List:
+            if str(x) == str(Choice):
+                del self.Alarm_List[Choice]
+                print("Alarm Removed")
+                print("Alarm List "+str(self.Alarm_List))
+                input("Press Anykey to Continue")
+                self.Menu()
+            else:
+                print("No alarm by That name Exists")
+                self.Menu()
+
     def Menu(self):
+        os.system('cls')
         print("Please Choose Option:")
         print(" 1.Current Time""\n 2.Add Alarm""\n 3.Remove Alarm""\n 4.Alarm List""\n 5.Set Alarms""\n 6.Quit")
         Choice = input()
         if Choice == "1":
             print(self.Current_Time_Update())
+            input("Press Anykey to Continue")
             self.Menu()
         elif Choice == "2":
             self.add_alarm()
+            input("Press Anykey to Continue")
             self.Menu()
         elif Choice == "3":
-            quit()
+            self.Remove_Alarm()
         elif Choice == "4":
             print(self.Alarm_List)
+            input("Press Anykey to Continue")
             self.Menu()
         elif Choice == "5":
-            Yeet.Set_Alarm()
+            self.Set_Alarm()
+            input("Press Anykey to Continue")
             self.Menu
         elif Choice == "6":
             quit
